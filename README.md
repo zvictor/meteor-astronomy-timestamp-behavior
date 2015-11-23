7bfdc8cbc29ccd4eaa3858922703a240e66c9367
zVictor <victor0110@msn.com>
2015-11-23 14:18:36 +0100

    publishes our fork as a package

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Fork from the original timestamp-behavior! It adds [#5](https://github.com/jagi/meteor-astronomy-timestamp-behavior/pull/5).
+
 # Timestamp behavior for Meteor Astronomy
 
 The `timestamp` behavior adds two fields that store information about document's creation and update dates.
--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   summary: 'Timestamp behavior for Meteor Astronomy',
   version: '1.1.0',
-  name: 'jagi:astronomy-timestamp-behavior',
-  git: 'https://github.com/jagi/meteor-astronomy-timestamp-behavior.git'
+  name: 'zvictor:astronomy-timestamp-behavior',
+  git: 'https://github.com/zvictor/meteor-astronomy-timestamp-behavior.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy@1.2.0');
+  api.use('zvictor:astronomy@1.2.0');
   api.use('underscore');
 
   // Behavior.
